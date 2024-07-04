1a2388b382f05178ff19c8c30847c45873ffd674
Reinaldo Silva <josereinaldo.pessoal@gmail.com>
2024-07-03 22:21:34 -0300

    feat: Contexto de finalização das sessões do banco de dados ao finalizar a execução da aplicação.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
 from uvicorn import run
+from contextlib import asynccontextmanager
+
+from app.core.database import close_db_connection
 from app.core.settings import settings
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Gerenciador de contexto para finalizar a conexão com o banco de dados
+    quando a aplicação é encerrada.
+
+    Args:
+        app (FastAPI): Instância da aplicação FastAPI.
+
+    """
+
+    try:
+        yield
+    finally:
+        await close_db_connection()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
