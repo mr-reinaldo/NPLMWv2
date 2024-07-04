3ed5d4f07caae64a2970baa834c61c6c6f415100
Reinaldo Silva <josereinaldo.pessoal@gmail.com>
2024-07-03 22:12:53 -0300

    feat: Implementada engine de conexão assíncrona com sqlalchemy

---
+++ backend/app/core/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.pool import NullPool
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from app.core.settings import settings
+
+
+engine: AsyncEngine = create_async_engine(
+    url=settings.database.database_url,
+    poolclass=NullPool,
+    echo=settings.database.echo,
+)
+
+session: AsyncSession = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False
+)
+
+
+async def close_db_connection() -> None:
+    await engine.dispose()
